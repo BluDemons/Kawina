cd7d9eefb09fd5a13360b112d9d3333d35855e32
Your Name <you@example.com>
2019-07-19 09:33:45 -0500

    components

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductComponent } from './product/product.component';
+import { ServicesComponent } from './services/services.component';
+import { SingleComponent } from './single/single.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/dashboard', pathMatch:'full'},
+  {path:'dashboard', component:DashboardComponent},
+  {path: 'empresa', component:AboutComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'product', component:ProductComponent},
+  {path:'services', component:ServicesComponent},
+  {path:'single', component:SingleComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductComponent } from './product/product.component';
+import { ServicesComponent } from './services/services.component';
+import { SingleComponent } from './single/single.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    ProductComponent,
+    ServicesComponent,
+    SingleComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,

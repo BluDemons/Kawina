8c3bb9eb2e1b171ae365407f93d120c99713be19
BluDemons <jar.salazar@yavirac.edu.ec>
2019-07-30 23:51:36 -0500

    mejorasheaders

--- Client/src/app/headers/headers.component.ts
+++ Client/src/app/headers/headers.component.ts
@@ -11,4 +11,13 @@ export class HeadersComponent implements OnInit {
 
   ngOnInit() {
   }
+  
+  responsive() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
 }

8be715f8b09477e3dbb1fde309e6a221575fd825
unknown <grarogccee@gmail.com>
2019-08-01 08:57:57 -0500

    sinerror

--- Client/src/app/login/login.component.ts
+++ Client/src/app/login/login.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-// import Swal from 'sweetalert2';
-=======
-//import Swal from 'sweetalert2';
->>>>>>> c5b4e62880f8f869731e2a03356efc55c0c3e6b1
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 // declare var $: any;
@@ -35,11 +30,7 @@ export class LoginComponent implements OnInit {
   });
   }
 
-<<<<<<< HEAD
-  // btnClickHome = function ()
-=======
-  // btnClickHome = function () {
->>>>>>> c5b4e62880f8f869731e2a03356efc55c0c3e6b1
+
     
   //   let validacionUser = $('#user').val();
   //   let validacionPass = $('#pass').val();
